523db01d5cc0af7f923749013d10542469653833
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2019-03-26 11:14:12 +0600

    Used Java8's DateTime class to display the current age when a person's date of birth is inserted. Wrote custom implementation of property accessor. Not storing the age as a separate field. We can check the age on the go.

--- Kotlin in Action/src/sas3/KotlinInAction/Ch02/Person.kt
+++ Kotlin in Action/src/sas3/KotlinInAction/Ch02/Person.kt
@@ -1,14 +1,29 @@
 package sas3.KotlinInAction.Ch02
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
 class Person(
         val name: String,
         val dateOfBirth: String)
 {
+    val currentAge:Int
+    get() {
+        var dateOfBirth = LocalDate.parse(dateOfBirth, DateTimeFormatter.ofPattern("MM/dd/yyyy"))
+        var dateToday = LocalDate.now()
+        return ChronoUnit.YEARS.between(dateOfBirth,dateToday).toInt()
+    }
+
 
 }
 
 
 fun main(args: Array<String>) {
-    val person: Person = Person("Shimon","24 Feb 1987")
+    val person = Person("Shimon","02/24/1987")
     println(person.toString())
+    println("The name is ${person.name} \n " +
+            "date of birth is ${person.dateOfBirth} \n" +
+            "current age is ${person.currentAge}"
+    )
 }
