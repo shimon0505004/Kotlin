412c93ec5640573c6e39f9529b12f8edc9d88654
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2019-03-27 02:17:43 +0600

    2.4.4: Checking range membership using "in"
    Signed-off-by: Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>

--- Kotlin in Action/src/sas3/KotlinInAction/Ch02/IterationOverIndex/IterationOverIndex.kt
+++ Kotlin in Action/src/sas3/KotlinInAction/Ch02/IterationOverIndex/IterationOverIndex.kt
@@ -2,15 +2,23 @@ package sas3.KotlinInAction.Ch02.IterationOverIndex
 
 
 fun iterationOverIndex() {
-
     val list = arrayListOf("10","11","1001")
 
+    /**
+     * iterated over a collection while keeping track of the
+     * index. We dont need to create seperate variable to store
+     * the index and increment it by hand.
+     */
     for((index, element) in list.withIndex()){
         println("${index} : ${element}")
     }
 }
 
+fun isLetter(c: Char) = c in 'a' .. 'z' || c in 'A' .. 'Z'
+fun isNotDigit(c: Char) = c !in '0' .. '9'
 
 fun main(args: Array<String>) {
     iterationOverIndex()
+    println(isLetter('q'))
+    println(isNotDigit('x'))
 }
