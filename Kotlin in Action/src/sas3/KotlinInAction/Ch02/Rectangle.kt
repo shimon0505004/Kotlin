62f27ef9b8d7df0167603184baf18a35419f311c
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2019-03-26 12:14:27 +0600

    Used package declaration and import statement with Rectangle class.

--- Kotlin in Action/src/sas3/KotlinInAction/Ch02/Rectangle.kt
+++ Kotlin in Action/src/sas3/KotlinInAction/Ch02/Rectangle.kt
@@ -1,8 +1,21 @@
 package sas3.KotlinInAction.Ch02
 
+import java.util.*
+
 class Rectangle(val height: Int, val width: Int) {
     val isSquare: Boolean
     get() {
         return height ==width
     }
+}
+
+fun createRandomRectangle(): Rectangle{
+    val random = Random()
+    return Rectangle(random.nextInt(),random.nextInt())
+}
+
+fun main(args: Array<String>) {
+    val randomRectangle = createRandomRectangle()
+    println("rectangle has ${randomRectangle.height} height " +
+            "${randomRectangle.width} width and is ${if(randomRectangle.isSquare) "" else "not"} a square")
 }
