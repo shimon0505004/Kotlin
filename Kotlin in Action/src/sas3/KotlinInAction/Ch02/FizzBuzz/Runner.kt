6a6bcc8a037617a9d87daa65da928067e3ea9f09
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2019-03-27 01:34:15 +0600

    2.4.2: Implemented FizzBuzz game using "when" and for loop in kotlin. Used downTo operator to define a range [1,100], inclusive and traverse the range from 100 to 1. Used step operator to skip some numbers.
    .. operator incremenally traverses the range.
    downTo operator decrementatlly traverses a given range.
    
    Signed-off-by: Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>

--- Kotlin in Action/src/sas3/KotlinInAction/Ch02/FizzBuzz/Runner.kt
+++ Kotlin in Action/src/sas3/KotlinInAction/Ch02/FizzBuzz/Runner.kt
@@ -1,7 +1,24 @@
 package sas3.KotlinInAction.Ch02.FizzBuzz
 
 fun main(args: Array<String>) {
+    /**
+     * .. operator in kotlin creates a closed range between two number,
+     * such a closed range is called a progression.
+     */
     for(number in 1..100){
+        println(fizzBuzz(number))
+    }
+
+    println("****-----***----****")
+    /**
+     * iterating over a range with a step
+     */
+    for(number in 100 downTo 1 step 2){
+        println(fizzBuzz(number))
+    }
+
+    println("****-----***----****")
+    for(number in 1 .. 100 step 2){
         println(fizzBuzz(number))
     }
 }
