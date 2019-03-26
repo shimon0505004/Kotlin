54d51f057e02077ad1edefa73765d5b58f1eec3d
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2019-03-26 13:53:02 +0600

    Added comments to Color enum class to note why we need semicolons.

--- Kotlin in Action/src/sas3/KotlinInAction/Ch02/Color.kt
+++ Kotlin in Action/src/sas3/KotlinInAction/Ch02/Color.kt
@@ -1,6 +1,11 @@
 package sas3.KotlinInAction.Ch02
 
 enum class Color (val r: Int, val g: Int, val b: Int){
+    /**
+     * Only place in kotlin where you need to use semicolon
+     * is in a enum class. This is necessary to seperate enum
+     * methods from enum constant lists.
+     */
     RED (255, 0, 0),
     ORANGE (255,165,0),
     YELLOW (255, 255, 0),
