18fde4e7e956ceb0460ec1990bdc41b8d9b0a2aa
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2019-03-26 15:44:54 +0600

    Added comments to explain imports when accessing enum constants without qualifier.

--- Kotlin in Action/src/sas3/KotlinInAction/Ch02OtherPackage/TestingEnumConstantImport.kt
+++ Kotlin in Action/src/sas3/KotlinInAction/Ch02OtherPackage/TestingEnumConstantImport.kt
@@ -1,5 +1,12 @@
 package sas3.KotlinInAction.Ch02OtherPackage
 
+/**
+ * We have to use two imports here, both .../color and .../color.*
+ * .../color enables us to access the enum class color, but not the enum values.
+ * to access the enum values, we need to explicitly import enum constants to
+ * use them by name. This is done using .../color.*
+ */
+
 import sas3.KotlinInAction.Ch02.Color
 import sas3.KotlinInAction.Ch02.Color.*
 
@@ -10,6 +17,12 @@ fun getWarmth(color: Color) = when(color){
 }
 
 fun main(args: Array<String>) {
+    /**
+     * We have two top level functions with same name in different package.
+     * If we dont explicitly mention the package, then it is the local
+     * package's top level function. To access the top level function of a
+     * different package, we need to prefix it with the package name.
+     */
     println(getWarmth(ORANGE))
     println(getWarmth(INDIGO))
     println(sas3.KotlinInAction.Ch02.getWarmth(ORANGE))
