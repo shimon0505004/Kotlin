1e1bc1ed5a2a29691c3c604ea1d773497c8eabb9
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2019-03-26 16:42:50 +0600

    Added comments to the SmartCasts example. After we initially check a variable for a type, we dont need to add an explicit cast to access members of that type. The compiler does this job for us.

--- Kotlin in Action/src/sas3/KotlinInAction/Ch02/SmartCasts/Eval.kt
+++ Kotlin in Action/src/sas3/KotlinInAction/Ch02/SmartCasts/Eval.kt
@@ -2,10 +2,19 @@ package sas3.KotlinInAction.Ch02.SmartCasts
 
 fun eval(expression : Expr): Int{
     if(expression is Num){
-        val n = expression as Num
+        val n = expression as Num       //This explicit cast to Num is redundant
         return n.value
     }
     if(expression is Sum){
+        /**
+         * The variable expression is smart-cast.
+         * Here, once we check the variable "expression" has Sum type,
+         * the compiler interprets it as a Num variable.
+         * We can then access the value property of Sum without an
+         * explicit cast.
+         * If we needed to do it in java, we would have needed to do explicit
+         * type casting.
+         */
         return eval(expression.right) + eval(expression.left)
     }
     throw IllegalArgumentException("Unknown Expression")
