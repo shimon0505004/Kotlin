ce6d8673cd5580257de726b5abc0a57a502e4616
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2019-03-26 12:21:52 +0600

    Testing if top level functions can be imported by name.

---
+++ Kotlin in Action/src/sas3/KotlinInAction/Ch02OtherPackage/TestingFunctionImport.kt
@@ -0,0 +1,7 @@
+package sas3.KotlinInAction.Ch02OtherPackage
+
+import sas3.KotlinInAction.Ch02.createRandomRectangle
+
+fun main(args: Array<String>) {
+    println(createRandomRectangle().isSquare)
+}
