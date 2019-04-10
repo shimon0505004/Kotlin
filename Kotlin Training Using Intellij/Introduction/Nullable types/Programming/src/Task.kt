17e2f1ff05a84c7155625d5d75e022ee0f0e524b
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2019-04-10 17:22:20 +0600

    Practiced nullable types for kotlin

--- Kotlin Training Using Intellij/Introduction/Nullable types/Programming/src/Task.kt
+++ Kotlin Training Using Intellij/Introduction/Nullable types/Programming/src/Task.kt
@@ -1,5 +1,7 @@
 fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
-    TODO()
+    val email = client?.personalInfo?.email
+    if(message!= null && email!=null)
+        mailer.sendMessage(email,message)
 }
 
 class Client (val personalInfo: PersonalInfo?)
